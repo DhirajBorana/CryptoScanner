bea3af4dc1ee170b6f69a24df45934b32a724d82
DhirajBorana <dhiraj.tarachand@gmail.com>
2022-07-09 13:27:19 +0530

    refined permissions logic & added click listener to open app's settings page.

--- app/src/main/java/com/example/cryptoscanner/CameraFragment.kt
+++ app/src/main/java/com/example/cryptoscanner/CameraFragment.kt
@@ -1,8 +1,11 @@
 package com.example.cryptoscanner
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -22,6 +25,7 @@ import com.example.cryptoscanner.databinding.FragmentCameraBinding
 import kotlinx.coroutines.launch
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import kotlin.math.log
 
 class CameraFragment : Fragment() {
 
@@ -53,14 +57,14 @@ class CameraFragment : Fragment() {
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        if (!allPermissionsGranted()) requestPermissionLauncher.launch(REQUIRED_PERMISSIONS)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        when {
-            allPermissionsGranted() ->  startCamera()
-            else -> requestPermissionLauncher.launch(REQUIRED_PERMISSIONS)
-        }
-
+        binding.cameraPermissionDeniedLayout.openSettingsBtn.setOnClickListener { openSettingsPage() }
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
@@ -88,6 +92,12 @@ class CameraFragment : Fragment() {
                 Log.e(TAG, "Use case binding failed", e)
             }
         }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun openSettingsPage() {
+        val uri: Uri = Uri.fromParts("package", activity?.packageName, null)
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).also { it.data = uri }
+        startActivity(intent)
     }
 
     private fun showCameraLayout() {
