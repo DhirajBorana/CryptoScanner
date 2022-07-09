d0c76c3f9068ffad152ec27d5011948913531e20
DhirajBorana <dhiraj.tarachand@gmail.com>
2022-07-09 13:09:50 +0530

    added code to start camera and QRCodeAnalyzer.kt to analyse the image frame for QR code.

--- app/src/main/java/com/example/cryptoscanner/CameraFragment.kt
+++ app/src/main/java/com/example/cryptoscanner/CameraFragment.kt
@@ -10,15 +10,25 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.cryptoscanner.databinding.FragmentCameraBinding
+import kotlinx.coroutines.launch
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraFragment : Fragment() {
 
     private val TAG = "CameraFragment"
     private lateinit var binding: FragmentCameraBinding
+    private lateinit var cameraExecutor: ExecutorService
+
     private val requestPermissionLauncher =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
             val granted = permissions.entries.all { it.value }
@@ -35,25 +45,6 @@ class CameraFragment : Fragment() {
             }
         }
 
-    private fun startCamera() {
-        showCameraLayout()
-        // TODO: Open Camera
-    }
-
-    private fun showCameraLayout() {
-        binding.apply {
-            cameraLayout.root.visibility = View.VISIBLE
-            cameraPermissionDeniedLayout.root.visibility = View.GONE
-        }
-    }
-
-    private fun showCameraPermissionDeniedLayout() {
-        binding.apply {
-            cameraLayout.root.visibility = View.GONE
-            cameraPermissionDeniedLayout.root.visibility = View.VISIBLE
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -68,6 +59,48 @@ class CameraFragment : Fragment() {
         when {
             allPermissionsGranted() ->  startCamera()
             else -> requestPermissionLauncher.launch(REQUIRED_PERMISSIONS)
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun startCamera() {
+        showCameraLayout()
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            val preview = Preview.Builder()
+                .build()
+                .also { it.setSurfaceProvider(binding.cameraLayout.viewFinder.surfaceProvider) }
+
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, QRCodeAnalyzer {
+                        // TODO: store scanned address
+                    })
+                }
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(viewLifecycleOwner, cameraSelector, preview, imageAnalyzer)
+            } catch (e: Exception) {
+                Log.e(TAG, "Use case binding failed", e)
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun showCameraLayout() {
+        binding.apply {
+            cameraLayout.root.visibility = View.VISIBLE
+            cameraPermissionDeniedLayout.root.visibility = View.GONE
+        }
+    }
+
+    private fun showCameraPermissionDeniedLayout() {
+        binding.apply {
+            cameraLayout.root.visibility = View.GONE
+            cameraPermissionDeniedLayout.root.visibility = View.VISIBLE
         }
     }
 
