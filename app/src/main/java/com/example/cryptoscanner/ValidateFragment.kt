a7d6f90e36a26205e0c51b6d6bf8a30b9b5352bb
DhirajBorana <dhiraj.tarachand@gmail.com>
2022-07-09 14:08:50 +0530

    added validation code for 'ethereum' & 'bitcoin' addresses.

--- app/src/main/java/com/example/cryptoscanner/ValidateFragment.kt
+++ app/src/main/java/com/example/cryptoscanner/ValidateFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
 import com.example.cryptoscanner.databinding.FragmentValidateBinding
+import java.util.regex.Pattern
 
 class ValidateFragment : Fragment() {
 
@@ -24,5 +25,21 @@ class ValidateFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.cryptoAddressTv.text = args.address
+        binding.validateBtn.setOnClickListener { isValidCryptoAddress(args.address) }
+    }
+
+    private fun isValidCryptoAddress(address: String): Boolean {
+        return when (args.cryptoType) {
+            CryptoType.ETH -> isValidEthereumAddress(address)
+            CryptoType.BTC -> isValidBitcoinAddress(address)
+        }
+    }
+
+    private fun isValidBitcoinAddress(address: String): Boolean {
+        return Pattern.matches("^[1][a-zA-HJ-KM-NP-Z1-9]{25,34}$", address)
+    }
+
+    private fun isValidEthereumAddress(address: String): Boolean {
+        return Pattern.matches("^[0x][0-9a-f]$", address)
     }
 }
