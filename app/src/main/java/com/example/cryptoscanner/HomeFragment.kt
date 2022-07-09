e1d6f98e988b824f28546dd28140b5603f1cca22
DhirajBorana <dhiraj.tarachand@gmail.com>
2022-07-09 12:14:08 +0530

    added fragment_camera to nav_graph.xml, and connected fragment_home to fragment_camera.

--- app/src/main/java/com/example/cryptoscanner/HomeFragment.kt
+++ app/src/main/java/com/example/cryptoscanner/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.cryptoscanner.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -18,5 +19,16 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.scanBtcBtn.setOnClickListener {
+            findNavController().navigate(HomeFragmentDirections.actionNavHomeToNavCamera())
+        }
+        binding.scanEthBtn.setOnClickListener {
+            findNavController().navigate(HomeFragmentDirections.actionNavHomeToNavCamera())
+        }
     }
 }
